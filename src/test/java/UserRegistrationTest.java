5cbd738f21e4fb90a8afd7f3e7a8ab0f49fa00b2
akshayzagad <akshayzagade62@gmail.com.com>
2022-12-27 23:07:16 +0530

    Junit test using mood analyser

--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -1,5 +1,37 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class UserRegistrationTest {
-    public static void main(String[] args) {
-        System.out.println("Welcome to User Registration Programme Test");
+    @Test
+    public void firstNameTest_ShouldReturnTrue() {
+        boolean value = UserRegistration.checkFirstName("Akshay");
+        //test output
+        assertEquals(true, value);
     }
+
+    @Test
+    public void lastNameTest2_ShouldReturnTrue() {
+        boolean value = UserRegistration.checkLastName("Zagade");
+        assertEquals(true, value);
+    }
+
+    @Test
+    public void emailTest3_ShouldReturnTrue() {
+        boolean value = UserRegistration.checkEmail("abc.xyz@gmail.com");
+        assertEquals(true, value);
+    }
+
+    @Test
+    public void numberTest4_ShouldReturnTrue() {
+        boolean value = UserRegistration.phoneNumber("91 9284046592");
+        assertEquals(true, value);
+    }
+
+    @Test
+    public void passWordTest5_ShouldReturnTrue() {
+        boolean value = UserRegistration.passWord("Yogesh@5213");
+        assertEquals(true, value);
+    }
+
 }
